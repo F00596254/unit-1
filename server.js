67457b5d3784f6ecb1cc350e05db99a230b21f16
Sravya C <csravya333@Sravyas-Air.attlocal.net>
2024-01-21 12:43:44 -0600

    Updated Add User code, added postman collection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
+var fs = require("fs")
+const { get } = require('http')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
 
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
@@ -9,21 +13,18 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-var user = {
-    "user4" : {
-       "name" : "mohit",
-       "password" : "password4",
-       "profession" : "teacher",
-       "id": 4
-    }
- }
  
  app.post('/addUser', function (req, res) {
+   const user = req.body;
     // First read existing users.
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
-       data["user4"] = user["user4"];
-       console.log( data );
+       data['user' + user.id] = user;
+       fs.writeFile(__dirname + "/" + "users.json", JSON.stringify(data), function(err, data) {
+         if (err) {
+            res.end('Incorrect payload');
+         }
+       })
        res.end( JSON.stringify(data));
     });
  })
@@ -55,5 +56,5 @@ var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    console.log(server.address());
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Unit-1 app listening at http://%s:%s", host, port)
 })
